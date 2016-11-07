fe48199498ce80d927b5d2334d0a632980582452
Keith Hon <keithhon@Keiths-MacBook-Pro-2.local>
2016-11-07 15:26:25 +0800

    Added .gitignore and created Xcode project.

---
+++ iOSUIImageExtension/iOSUIImageExtension.h
@@ -0,0 +1,13 @@
+//
+//  iOSUIImageExtension.h
+//  iOSUIImageExtension
+//
+//  Created by Keith Hon on 11/7/16.
+//  Copyright © 2016 Keith Hon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSUIImageExtension : NSObject
+
+@end
---
+++ iOSUIImageExtension/iOSUIImageExtension.m
@@ -0,0 +1,13 @@
+//
+//  iOSUIImageExtension.m
+//  iOSUIImageExtension
+//
+//  Created by Keith Hon on 11/7/16.
+//  Copyright © 2016 Keith Hon. All rights reserved.
+//
+
+#import "iOSUIImageExtension.h"
+
+@implementation iOSUIImageExtension
+
+@end
