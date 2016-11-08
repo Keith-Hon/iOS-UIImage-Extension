a2e3354cf15358de2076eff487908c10edd3a613
Keith Hon <keithhon@Keiths-MacBook-Pro-2.local>
2016-11-08 14:09:55 +0800

    Moved comments of methods to header file

--- iOSUIImageExtension/iOSUIImageExtension.h
+++ iOSUIImageExtension/iOSUIImageExtension.h
@@ -11,10 +11,13 @@
 
 @interface UIImage (UIImageExtension)
 
+// Initiailze an UIImage object with a byte array, whose memory will be released after initialization if releaseData flag is set to YES
 -(id) initWithBytes :(unsigned char*) data image_height:(int)height image_width:(int) width numberOfComponents:(int) numberOfComponents releaseData:(bool) releaseData setAlpha:(bool) setAlpha;
 
+// Initiailze an UIImage object with a float array, whose memory will be released after initialization if releaseData flag is set to YES
 -(id) initWithFloats:(float*) data image_height:(int)height image_width:(int) width numberOfComponents:(int) numberOfComponents releaseData:(bool) releaseData setAlpha:(bool) setAlpha;
 
+// Get a byte array copy from an UIImage. The return array contains four channel with alpha value at the last channel if withAlpha is true and 255 otherwise
 -(unsigned char*) getBytes: (bool)withAlpha;
 
 @end
--- iOSUIImageExtension/iOSUIImageExtension.mm
+++ iOSUIImageExtension/iOSUIImageExtension.mm
@@ -24,7 +24,6 @@
     
 }
 
-// Initiailze an UIImage object with a byte array, whose memory will be released after initialization if releaseData flag is set to YES
 -(id) initWithBytes: (unsigned char*) data image_height:(int)height image_width:(int) width numberOfComponents:(int) numberOfComponents releaseData:(bool) releaseData setAlpha:(bool) setAlpha{
     
     [self checkNumberOfComponents: numberOfComponents];
@@ -70,7 +69,6 @@
 
 }
 
-// Initiailze an UIImage object with a float array, whose memory will be released after initialization if releaseData flag is set to YES
 -(id) initWithFloats: (float*) data image_height:(int)height image_width:(int) width numberOfComponents:(int) numberOfComponents releaseData:(bool) releaseData setAlpha:(bool) setAlpha{
     
     [self checkNumberOfComponents: numberOfComponents];
@@ -109,7 +107,6 @@
     
 }
 
-// Get byte array from an UIImage. The return array contains four channel with alpha value at the last channel if withAlpha is true and 255 otherwise
 -(unsigned char*) getBytes: (bool)withAlpha {
     
     // Output data always has four channels
